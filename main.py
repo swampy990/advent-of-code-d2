5db8c26054e96bf29a33117579f207f1466eeb16
Craig Ballingall <craig.ballingall@gmail.com>
2022-12-02 17:26:15 +0000

    completed part 2

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 
+"""
+X requires a loss       A=Rock    1 Point       Beaten by Paper
+Y requires a draw       B=Paper   2 Point      Beaten by Scissors
+Z requires a win        C=Scissors 3 Point      Beaten by Rock
+
+"""
 # Create a function to determine the score
-def score(opponent, me):
-    if opponent == 'A' and me == 'Z':
-        return 3
-    elif opponent == 'B' and me == 'X':
-        return 1
-    elif opponent == 'C' and me == 'Y':
-        return 2
-    elif opponent == 'A' and me == 'X':
-        return 4
-    elif opponent == 'B' and me == 'Y':
-        return 5
-    elif opponent == 'C' and me == 'Z':
-        return 6
-    elif opponent == 'A' and me == 'Y':
+def winner(opponent):
+    if opponent == 'A': #Paper beats rock
         return 8
-    elif opponent == 'B' and me == 'Z':
+    elif opponent == 'B': #Scissors beat paper
         return 9
-    elif opponent == 'C' and me == 'X':
+    elif opponent == 'C': #Rock beats scissors
         return 7
-    return False
+
+
+def drawn(opponent):
+    if opponent == 'A':
+        return 4
+    if opponent == 'B':
+        return 5
+    if opponent == 'C':
+        return 6
+
+
+def loss(opponent):
+    if opponent == 'A':
+        return 3
+    if opponent == 'B':
+        return 1
+    if opponent == 'C':
+        return 2
+
 
 data = []
 for line in open("strat.txt", "r"):
@@ -27,6 +39,11 @@ for line in open("strat.txt", "r"):
     O = split[0]
     M = split[1]
 
-    data.append(score(O, M))
+    if M == 'X':
+        data.append(loss(O))
+    elif M == 'Y':
+        data.append(drawn(O))
+    elif M == 'Z':
+        data.append(winner(O))
 
 print(sum(data))
